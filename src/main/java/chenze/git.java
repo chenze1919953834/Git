df64c2874e8b20bc2c59a35c87d0247c0c0620dc
chenze1919953834 <87766137+chenze1919953834@users.noreply.github.com>
2021-07-23 21:40:22 +0800

    Update git.java

--- src/main/java/chenze/git.java
+++ src/main/java/chenze/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("push test!");
+        System.out.println("push test2!");
     }
 }
